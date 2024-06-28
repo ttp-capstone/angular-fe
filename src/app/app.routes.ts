fe642b6da5cc5038975a1f91946f5770317c9fe0
akshay45 <akshayzymbo79@gmail.com>
2024-06-28 13:15:08 -0400

    Dashboard slight changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -114,6 +114,12 @@ export const routes: Routes = [
       data: {
         title: 'Register Page'
       }
+    },{
+      path: '404',
+      loadComponent: () => import('./public/pages/page404/page404.component').then(m => m.Page404Component),
+      data: {
+        title: 'Page 404'
+      }
     },
     
     // { path: '', redirectTo: '/', pathMatch: 'full' },
