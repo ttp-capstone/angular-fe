7b76c1be617911d343f23bcc9844b2118682fb4f
akshay45 <akshayzymbo79@gmail.com>
2024-07-23 08:49:22 -0400

    Add-Fund-Table

--- src/app/customer/funding/applied-funding/applied-funding.component.ts
+++ src/app/customer/funding/applied-funding/applied-funding.component.ts
@@ -1,44 +1,46 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { CommonModule, NgTemplateOutlet } from '@angular/common';
-import { DocsExampleComponent } from '@docs-components/public-api';
-import { BorderDirective, ButtonDirective,CardBodyComponent,CardComponent,CardFooterComponent,CardGroupComponent,CardHeaderComponent,CardImgDirective,CardLinkDirective,
-CardSubtitleDirective,CardTextDirective,CardTitleDirective,ColComponent,GutterDirective,ListGroupDirective,ListGroupItemDirective,NavComponent,
-NavItemComponent,NavLinkDirective,RowComponent,TextColorDirective} from '@coreui/angular';
-import { ActivatedRoute, RouterModule, Router } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { RowComponent, ColComponent, CardComponent, CardHeaderComponent, CardBodyComponent, CardFooterComponent, ButtonDirective, TableDirective, TextColorDirective } from '@coreui/angular';
 import { FundingService } from 'src/app/service/funding.service';
 
 @Component({
   selector: 'app-applied-funding',
   standalone: true,
-  imports: [CommonModule, RouterModule, RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, DocsExampleComponent, NgTemplateOutlet, CardTitleDirective, CardTextDirective, ButtonDirective, CardSubtitleDirective, CardLinkDirective, RouterLink, ListGroupDirective, ListGroupItemDirective, CardFooterComponent, NavComponent, NavItemComponent, NavLinkDirective, BorderDirective, CardGroupComponent, GutterDirective, CardImgDirective],
+  imports: [
+    CommonModule,
+    RouterModule,
+    RowComponent,
+    ColComponent,
+    CardComponent,
+    CardHeaderComponent,
+    CardBodyComponent,
+    CardFooterComponent,
+    ButtonDirective,
+    TableDirective,
+    TextColorDirective
+  ],
   templateUrl: './applied-funding.component.html',
-  styleUrl: './applied-funding.component.scss'
+  styleUrls: ['./applied-funding.component.scss']
 })
 export class AppliedFundingComponent {
   funding: any[] = [];
-  constructor(
-    private service: FundingService,
-    private router: Router,
-    private route: ActivatedRoute,
-  ) { }
+
+  constructor(private service: FundingService) { }
 
   ngOnInit(): void {
     this.fetchAllAppliedFunding();
   }
 
   fetchAllAppliedFunding() {
-   
     this.service.fetchAllAppliedFunding().subscribe(
       (funding) => {
-        this.funding = funding; // Assuming projects is an array of project objects
+        this.funding = funding; // Ensure this matches the data structure
       },
       (error) => {
-        console.error('Could not fetch projects', error);
-        // Handle error appropriately, e.g., show error message to user
+        console.error('Could not fetch funding', error);
       }
     );
-  
+  }
 }
-}
+
